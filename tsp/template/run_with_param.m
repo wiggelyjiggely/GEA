983590998610ecdb7cf2da5cd79dcaf283edcf1a
wiggelyjiggely <wiggelyjiggely@hotmail.com>
2019-11-13 17:31:16 +0100

    fdf

--- tsp/template/run_with_param.m
+++ tsp/template/run_with_param.m
@@ -1,7 +1,7 @@
 function run_with_param()
 
-NIND=20;		% Number of individuals
-MAXGEN=1000;		% Maximum no. of generations
+NIND=50;		% Number of individuals
+MAXGEN=100;		% Maximum no. of generations
 NVAR=26;		% No. of variables
 PRECI=1;		% Precision of variables
 ELITIST=0.95;    % percentage of the elite population
@@ -26,13 +26,13 @@ NVAR=size(data,1);
 figure;
 title("Population: " + NIND);
 hold on;
-for jump1= [1,3,5,10]
-        PR_MUT = jump1/100;
+for jump1= [90,95,97,99]
+        PR_CROSS = jump1/100;
         [fit,min,best] = cust_run(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR_MUT, CROSSOVER, LOCALLOOP);
         %cust_visualize_results(best,fit);
         sparsemean = sparse(fit);
         sizesparse = nnz(sparsemean);
-        plot([1:1:sizesparse],sparsemean(1:sizesparse),'DisplayName',"Mutaion rate: " + num2str(PR_MUT));
+        plot([1:1:sizesparse],sparsemean(1:sizesparse),'DisplayName',"Crossover rate: " + num2str(PR_CROSS));
         xlabel("Generation");
         ylabel("Mean fitness value");
 end
