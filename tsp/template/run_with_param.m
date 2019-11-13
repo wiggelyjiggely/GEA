f20d6642115367d4c01c21adb72c7b3430bbeebb
Vandebeek Sjaan <sjaanvandebeek.dpr@gmail.com>
2019-11-13 17:30:09 +0100

    stop

--- tsp/template/run_with_param.m
+++ tsp/template/run_with_param.m
@@ -1,12 +1,17 @@
 function run_with_param()
 
-NIND=20;		% Number of individuals
-MAXGEN=1000;		% Maximum no. of generations
+NIND=50;		% Number of individuals
+MAXGEN=100;		% Maximum no. of generations
 NVAR=26;		% No. of variables
 PRECI=1;		% Precision of variables
 ELITIST=0.95;    % percentage of the elite population
 GGAP=1-ELITIST;		% Generation gap
 STOP_PERCENTAGE=1;    % percentage of equal fitness individuals for stopping
+STOP_GEN_AVERAGE = 5; % amount of generations the average fitness does not change
+STOP_PERCENTAGE_AVERAGE = 0.01; % percentage of change for the average fitness
+STOP_GEN_BEST = 5; % amount of generations the best fitness does not change
+STOP_PERCENTAGE_BEST = 0.01; % percentage of change for the best fitness
+STOP_PERCENTAGE_RATIO = 0.01; % percentage for the ratio of the average and best
 PR_CROSS=.95;     % probability of crossover
 PR_MUT=.05;       % probability of mutation
 LOCALLOOP=1;      % local loop removal
@@ -26,16 +31,17 @@ NVAR=size(data,1);
 figure;
 title("Population: " + NIND);
 hold on;
-for jump1= [1,3,5,10]
-        PR_MUT = jump1/100;
-        [fit,min,best] = cust_run(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR_MUT, CROSSOVER, LOCALLOOP);
+%for jump1= [1,3,5,10]
+        %PR_MUT = jump1/100;
+        %[fit,min,best] = cust_run(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, PR_CROSS, PR_MUT, CROSSOVER, LOCALLOOP);
+        [fit,min,best] = run_ga_with_stop(x, y, NIND, MAXGEN, NVAR, ELITIST, STOP_PERCENTAGE, STOP_GEN_AVERAGE, STOP_PERCENTAGE_AVERAGE, STOP_GEN_BEST, STOP_PERCENTAGE_BEST, STOP_PERCENTAGE_RATIO, PR_CROSS, PR_MUT, CROSSOVER, LOCALLOOP);
         %cust_visualize_results(best,fit);
         sparsemean = sparse(fit);
         sizesparse = nnz(sparsemean);
         plot([1:1:sizesparse],sparsemean(1:sizesparse),'DisplayName',"Mutaion rate: " + num2str(PR_MUT));
         xlabel("Generation");
         ylabel("Mean fitness value");
-end
+%end
 hold off;
 legend show;
 end
